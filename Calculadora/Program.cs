956f68bc714c2016731fd95d44266d01e5e42b46
FelipeSousaP <comffelipe@gmail.com>
2025-07-11 19:47:12 -0300

    Teste

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Calculo;
 
-//variaveis
 int N1 = 0;
 int N2 = 0;
 bool continuar = true;
@@ -14,18 +13,30 @@ while (continuar)
     Console.Write("\nPorfavor, Digite o numero inteiro: ");
     while (!int.TryParse(Console.ReadLine(),out N2)) {Console.Write("\nPor favor digite corretamente: ");}
 
-    Console.WriteLine("A = adição;\nS = Subtração;\nD = Divisão;\nM = Multiplicação");
+    Console.WriteLine("\nA = adição;\nS = Subtração;\nD = Divisão;\nM = Multiplicação");
     Console.Write("Operação:");
-/*
-    OperaçãoEscolhida = Console.ReadKey().Key;
 
-    if(OperaçãoEscolhida == ConsoleKey.A)
-        operações.Soma(N1, N2);
-    else if(OperaçãoEscolhida == ConsoleKey.S)
-        operações.Subtração(N1, N2);
-    else if(OperaçãoEscolhida == ConsoleKey.D)
-        operações.Divisão(N1, N2);
-    else if(OperaçãoEscolhida == ConsoleKey.M)
-        operações.Multiplicação(N1, N2);
-*/
+    //O "ConsoleKey" tem um problema, ele utiliza o teclado como biblioteca, ou seja dependendo do modelo do computador ou forma de Abnt
+    //O programa não funciona, pois o codigo não considera, além disso, ConsoleKey aparentemente le tudo em Upper.
+    char OperaçãoEscolhida = Console.ReadKey().KeyChar;
+    OperaçãoEscolhida = char.ToUpper(OperaçãoEscolhida);
+
+    switch (OperaçãoEscolhida)
+    {
+        case 'A':
+            int resultado = operações.Soma(N1, N2);
+            break;
+        case 'S':
+            int resultado2 = operações.Subtração(N1, N2);
+            break;
+        case 'D':
+            int resultado3 = operações.Divisão(N1, N2);
+            break;
+        case 'M': 
+            int resultado4 = operações.Multiplicação(N1, N2);
+            break;
+        default:
+            Console.WriteLine("\nPor favor, Digite Corretamente!!!");
+            break;
+    }
 }
