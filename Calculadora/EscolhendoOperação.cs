4bdb734c73bf2bf00599f171255c7485700ff937
FelipeSousaP <comffelipe@gmail.com>
2025-07-14 21:52:29 -0300

    FEAT: Exibidor de tabuada

---
+++ Calculadora/EscolhendoOperação.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Calculo;
+
+namespace Calculo.Escolha
+{
+    public class EscolhendoOperação
+    {
+        public Operações operações1 { get; set; }
+        public Dictionary<string, Operações> Escolhas {  get; set; }
+        public void EscolherOperação()
+        {
+            operações1 = new Operações();
+        }
+    }
+}
---
+++ TabuadaExibida/Program.cs
@@ -0,0 +1,13 @@
+﻿//O exercicio apenas pede exibição.
+
+for(int i = 2;  i < 10; i++) //Essa repetição vai 1 vez
+{
+    Console.WriteLine($"\n*********************\nTabuada de {i}\n*********************");
+    for(int j = 0; j < 11; j++) //Essa repetição vai por total
+    {
+        int total = i * j; 
+        Console.WriteLine($"{i} * {j} = {total}");
+    }
+    // Assim fazendo tudo denovo
+}
+
