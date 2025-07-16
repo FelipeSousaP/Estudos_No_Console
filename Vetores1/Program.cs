126cc338717a6bb3644222d4d8e72395b96cab37
FelipeSousaP <comffelipe@gmail.com>
2025-07-16 10:27:47 -0300

    FEAT: vetores, primeior exercicio

---
+++ Vetores1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] soma = new int[10];
+int novasoma = 0;
+
+for(int i = 0; i < soma.Length; i++)
+{
+    Console.Write("\nPoderia escrever um numero? ");
+    int receba = int.Parse(Console.ReadLine()); //Pedi o numero;
+    soma[i] = receba; //Colocando o numero pedido na array
+    novasoma += soma[i]; //Somando o numero pedido com a soma passada
+    Console.WriteLine($"A soma atual é {novasoma}"); //Exibindo
+}
+
